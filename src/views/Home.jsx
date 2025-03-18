0175631892e6ca421dbe75673877c06a48c0f3ef
dia-avv <eaa24avv@students.eaaa.dk>
2025-03-18 21:29:47 +0100

    logo fix

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <>
-        <img src="../public/logo.png" alt="logo" style={{width: "200px", height: "auto"}}/>
+        <img src="/logo.png" alt="logo" style={{width: "200px", height: "auto"}}/>
         <h1>Dog Breed Identification</h1>
         <p>Have you ever visited someone’s house and saw that they had a really nice looking dog and wanted one for yourself? <br></br>Now you can find out what it is without asking them! 🐶💕</p>
         </>
